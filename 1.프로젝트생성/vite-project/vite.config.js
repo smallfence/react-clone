da31361726d42cf5fe3eaca45f6bdbc48ffe1e06
wondaehoon <dhwon234@naver.com>
2024-07-08 09:27:54 +0900

    [fix] hello world

--- 1.프로젝트생성/vite-project/vite.config.js
+++ 1.프로젝트생성/vite-project/vite.config.js
@@ -11,5 +11,8 @@ export default defineConfig({
       "!@#":path.resolve(__dirname,"./src")
       // 위 코드는 이것과 같은거임. "!@#":"C:/zdhwon/01개인_04사이드,클론,토이프로젝트/02클론프로젝트/react/1.프로젝트생성/vite-project/src"
     }
+  },
+  server: {
+    port: 3000
   }
 })
